a2c41edca7bb39325f5f712215432da8c622f1af
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2024-02-03 03:34:29 +0300

    refactorings done according to new like response and post response classes

--- src/main/java/com/project/questapp/business/abstracts/LikeService.java
+++ src/main/java/com/project/questapp/business/abstracts/LikeService.java
@@ -2,15 +2,16 @@ package com.project.questapp.business.abstracts;
 
 import com.project.questapp.entities.Like;
 import com.project.questapp.entities.Post;
+import com.project.questapp.reponses.LikeResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface LikeService  {
-    List<Like> findAllLikes(Optional<Long> userId, Optional<Long> postId);
-    List<Like> findAllByUserId(Long userId);
-    List<Like> findAllByPostId(Long postId);
+    List<LikeResponse> findAllLikes(Optional<Long> userId, Optional<Long> postId);
+    List<LikeResponse> findAllByUserId(Long userId);
+    List<LikeResponse> findAllByPostId(Long postId);
     void add(Like like);
 
     void delete(long id);
