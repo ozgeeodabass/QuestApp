a60d1fadf0cf1d2fe222b4c11835fe9d9e4092b3
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2024-03-13 03:23:18 +0300

    @Service anotation added

--- src/main/java/com/project/questapp/business/concretes/UserDetailsServiceImpl.java
+++ src/main/java/com/project/questapp/business/concretes/UserDetailsServiceImpl.java
@@ -6,7 +6,9 @@ import com.project.questapp.security.JwtUserDetails;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
    private UserRepository userRepository;
