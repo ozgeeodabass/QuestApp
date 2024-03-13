7a8dc7e9cb490cf6be3d5a45004c68b5bae8b031
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2024-03-13 03:18:37 +0300

    loadUserByName method implemented

--- src/main/java/com/project/questapp/business/concretes/UserDetailsServiceImpl.java
+++ src/main/java/com/project/questapp/business/concretes/UserDetailsServiceImpl.java
@@ -1,12 +1,22 @@
 package com.project.questapp.business.concretes;
 
+import com.project.questapp.dataAccess.UserRepository;
+import com.project.questapp.entities.User;
+import com.project.questapp.security.JwtUserDetails;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+   private UserRepository userRepository;
+
+   public UserDetailsServiceImpl(UserRepository userRepository){
+       this.userRepository=userRepository;
+   }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+       User user = userRepository.findByUserName(username);
+       return JwtUserDetails.create(user);
     }
 }
