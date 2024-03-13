7757fa6731e31f81d535d6bfa776f71241a83573
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2024-03-13 16:43:07 +0300

    AuthController implemented

---
+++ src/main/java/com/project/questapp/api/controllers/AuthController.java
@@ -0,0 +1,45 @@
+package com.project.questapp.api.controllers;
+
+import com.project.questapp.business.abstracts.UserService;
+import com.project.questapp.business.concretes.UserManager;
+import com.project.questapp.entities.User;
+import com.project.questapp.requests.UserRequest;
+import com.project.questapp.security.JwtTokenProvider;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private AuthenticationManager authenticationManager;
+    private JwtTokenProvider jwtTokenProvider;
+
+    private UserService userService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody UserRequest loginRequest){
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(loginRequest.getUserName(),loginRequest.getPassword());
+        Authentication auth = authenticationManager.authenticate(authToken);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        String jwtToken = jwtTokenProvider.generateJwtToken(auth);
+        return "Bearer "+jwtToken;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody UserRequest registerRequest){
+        if(userService.findByUserName(registerRequest.getUserName())!=null)
+            return new ResponseEntity<>("Username already in use", HttpStatus.BAD_REQUEST);
+
+        User user = new User();
+        user.setUserName(registerRequest.getUserName());
+        user.setPassword(registerRequest.getPassword());
+        userService.add(user);
+        return new ResponseEntity<>("You successfully registered!", HttpStatus.CREATED);
+    }
+}
