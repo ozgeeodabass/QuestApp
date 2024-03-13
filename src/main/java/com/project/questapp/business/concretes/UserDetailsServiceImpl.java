9151868eacfe6ccc377ffe40b03067d29564fc89
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2024-03-13 03:15:27 +0300

    UserDetailsService Implementation class created

---
+++ src/main/java/com/project/questapp/business/concretes/UserDetailsServiceImpl.java
@@ -0,0 +1,12 @@
+package com.project.questapp.business.concretes;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
